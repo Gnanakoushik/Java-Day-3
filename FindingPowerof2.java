19f143ad6cf410f800da258afc47289588db3bae
Gnanakoushik <jpranathi04@gmail.com>
2024-12-04 15:02:32 +0530

    Add files via upload

---
+++ FindingPowerof2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FindingPowerof2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=(1<<n);
+        System.out.println(c);
+
+    
+    }
+}
---
+++ Set.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Set{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int pos=sc.nextInt();
+        if((a&(1<<pos))!=0)
+        System.out.println("Set Bit");
+        else
+        System.out.println("Not a set bit");
+    }
+}
